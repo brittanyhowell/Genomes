571708367c67debbe31c65d24ede51b7dd433583
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-05-19 12:42:04 +0930

    New files and modifications indexSTARH/M are scripts designed to index the human and mouse genomes. runSTAR is an up to date STAR which runs STAR on two single reads in the VM runSTARTestVM serves a similar purpose unzip.sh was used to unzip all files in any folder

--- runSTAR.sh
+++ runSTAR.sh
@@ -1,40 +1,8 @@
 #!/bin/bash
 
-# Invoked by:
-#
-# sbatch indexSTAR.sh 
-# RUN THIS FROM THE SCRIPTS FOLDER. IT RELIES ON A RELATIVE PATH!!!
-
-#SBATCH -p batch
-#SBATCH -N 1 
-#SBATCH -n 8  				
-#SBATCH --time=0-04:00  	
-#SBATCH --mem=30GB 			
-
-# Notification configuration 
-#SBATCH --mail-type=END                                         
-#SBATCH --mail-type=FAIL           
-#SBATCH --mail-user=brittany.howell@student.adelaide.edu.au      
+STAR --genomeDir ../mouse/indexed/ --readFilesIn Mut-F2-Rep1_CGTACG_L007_R1.fastq Mut-F2-Rep1_CGTACG_L007_R2.fastq --outFilterMismatchNmax 2 --outFilterMultimapNmax 20 --outFileNamePrefix ../mouse/output/mm10 --outSAMstrandField intronMotif --outSAMtype BAM SortedByCoordinate --alignSoftClipAtReferenceEnds No 
 
 
-multiMap=("20" "25" "30" "35" "40" "45")
-mismatch=("2" "5" "10" "15" "20")
 
-# Load the necessary modules
-module load STAR/2.5.1a-foss-2015b
 
-# build with STAR
-for iMap in "${multiMap[@]}"; do
-for iMatch in "${mismatch[@]}"; do
-
-STAR --genomeDir ../mouse/indexed/ --readFilesIn /data/rc003/Brittany/Data/TranscriptomeData/Henmt1_mRNAseq_mouse/raw_data/read1/ /data/rc003/Brittany/Data/TranscriptomeData/Henmt1_mRNAseq_mouse/raw_data/read2 --outFilterMismatchNmax ${mismatch} --outFilterMultimapNmax ${multiMap} --outFileNamePrefix ../mouse/output/mm10 --outSAMstrandField intronMotif --outSAMattributes All --outSAMtype BAM SortedByCoordinate --alignSoftClipAtReferenceEnds No 
-
-2> outputSTAR.log
-
-done 
-done
-
-# outSAMstrandField intronMotif:Allows compatability with Cufflinks
-# outSAMattributes All: Provides all SAM attributes
-# outSAMtype BAM SortedByCoordinate: Sorts output by coordinate
-# alignSoftClipAtReferenceEnds: prohibits soft clipping of alignments past ends of chromosomes, useful for cufflinks compatibility
+# Removed outSAMattributes All - because I'm not sure it "All" is correct
---
+++ unzip.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+for file in *; 
+do unzip ${file} 
+done 

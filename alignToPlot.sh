d8d884279079b31634c874e2cced0fd1fe7370d7
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-06-01 14:58:02 +0930

    VM ready changes Several additions have been made. Firstly, the directories have been changed to reflect those on the VM. Next, several if statements have been created to check that all directories exist, and creates them if necessary. The STAR mismatch and multimap flags have been changed to the more correct, iMap/iMatch, rather than the entire vector. A file has been created for progress tracking called "record.txt". Every echo statement appends a line to record.txt, so that the progress of the script can be monitored. Once the alignment has been processed, the three ".out" files will be moved into the record folder. Finally, once all parameters have been cycled through so all alignments are finished for that read, I get an email saying that the process has finished, and get sent a copy of record.txt

--- alignToPlot.sh
+++ alignToPlot.sh
@@ -2,44 +2,101 @@
 ## Date: 31-5-2016
 
 # Working folders
-rawDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/Data/TranscriptomeData/Henmt1_mRNAseq_mouse/raw_data
-indexedDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/Data/indexedGenomes/indexedMouse/
-alignDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/Data/TranscriptomeData/Henmt1_mRNAseq_mouse/aligned_reads
-alignOutDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/Data/TranscriptomeData/Henmt1_mRNAseq_mouse/AlignmentOutputData
+rawDIR=/mnt/project/Data/Mouse/rawReads
+indexedDIR=/mnt/project/Data/Mouse/indexedMouse
+alignDIR=/mnt/project/Data/Mouse/alignedReads
+alignOutDIR=/mnt/project/Data/Mouse/AlignmentOutputData
+record=/mnt/project/Data/Mouse/record.txt 
+
+if [ -f ${record} ]; then
+	rm ${record}
+	touch ${record}
+    echo "${record} exists, replacing" >> ${record} 2>&1
+else
+    touch ${record}
+    echo "${record} did not exist, now does" >> ${record} 2>&1
+fi
+
+
+echo "The program starts at:" >> ${record} 2>&1
+date >> ${record} 2>&1
+
+# Check all folders exist
+
+if [ -d $rawDIR ]; then
+    echo "Folder $rawDIR exists ..." >> ${record} 2>&1
+else
+    mkdir $rawDIR
+    echo "Folder $rawDIR does not exist, making $rawDIR" >> ${record} 2>&1
+fi
+
+if [ -d $indexedDIR ]; then
+    echo "Folder $indexedDIR exists ..." >> ${record} 2>&1
+else
+    mkdir $indexedDIR
+    echo "Folder $indexedDIR does not exist, making $indexedDIR" >> ${record} 2>&1
+fi
+
+if [ -d $alignDIR ]; then
+    echo "Folder $alignDIR exists ..." >> ${record} 2>&1
+else
+    mkdir $alignDIR
+    echo "Folder $alignDIR does not exist, making $alignDIR" >> ${record} 2>&1
+fi
+
+if [ -d $alignOutDIR ]; then
+    echo "Folder $alignOutDIR exists ..." >> ${record} 2>&1
+else
+    mkdir $alignOutDIR
+    echo "Folder $alignOutDIR does not exist, making $alignOutDIR" >> ${record} 2>&1
+fi
 
 #Alignment parameters 
 MULTIMAP=("20" "25" "30" "35" "40" "45")
 MISMATCH=("2" "5" "10" "15" "20")
 
+echo "Commencing alignments" >> ${record} 2>&1
+date >> ${record} 2>&1
+
 # Move into reads folder
 cd ${rawDIR}
 READ=$(ls *_R1.fastq.gz)
 
-for file in ${READ}
-do
+for file in ${READ}; do
+
 	for iMap in "${MULTIMAP[@]}"; do
 
 		for iMatch in "${MISMATCH[@]}"; do
 
 			readname=${file%_R1.fastq.gz}
 
-				
-
-
+			echo "Running STAR for ${readname} with ${iMap} multimapping and ${iMatch} mismatches" >> ${record} 2>&1
+			date >> ${record} 2>&1
 
 			#Run STAR
-			STAR --genomeDir ${indexedDIR} --readFilesIn ${rawDIR}/${readname}_R1.fastq.gz ${rawDIR}/${readname}_R2.fastq.gz --outFilterMismatchNmax ${MISMATCH} --outFilterMultimapNmax ${MULTIMAP} --outFileNamePrefix ${alignDIR}/${readname}.STAR. --outSAMstrandField intronMotif --outSAMattributes All --outSAMtype BAM SortedByCoordinate --alignSoftClipAtReferenceEnds No 
+			STAR --genomeDir ${indexedDIR} --readFilesIn ${rawDIR}/${readname}_R1.fastq.gz ${rawDIR}/${readname}_R2.fastq.gz --outFilterMismatchNmax ${iMatch} --outFilterMultimapNmax ${iMap} --outFileNamePrefix ${alignDIR}/${readname}.STAR. --outSAMstrandField intronMotif --outSAMattributes All --outSAMtype BAM SortedByCoordinate --alignSoftClipAtReferenceEnds No  \ 
+	 2> ${record} 
 
+	 		cd ${alignDIR}
 			# Make bam index
-			samtools index ${readname}.STAR.Aligned.sortedByCoord.out.bam
+			samtools index ${readname}.STAR.Aligned.sortedByCoord.out.bam 2> ${record} 
 
 			# Move alignment information files into separate folder
-			mv ${readname}.STAR.Log.out ${alignOutDIR}
-			mv ${readname}.STAR.Log.final.out ${alignOutDIR}
-			mv ${readname}.STAR.Log.progress.out ${alignOutDIR}
+			mv ${readname}.STAR.Log.out ${alignOutDIR} 2> ${record}
+			mv ${readname}.STAR.Log.final.out ${alignOutDIR} 2> ${record}
+			mv ${readname}.STAR.Log.progress.out ${alignOutDIR} 2> ${record}
+
+			echo "Completed alignment for ${readname} with ${iMap} multimapping and ${iMatch} mismatches" >> ${record} 2>&1
+			date >> ${record} 2>&1
+
+			cd ${rawDIR}
+
 		done
 
 	done
 
+	mail -s "${READ} finished" brittany.howell@student.adelaide.edu.au < record=/mnt/project/Data/Mouse/record.txt 
+
 done
 
+echo "Exiting program" >> ${record} 2>&1

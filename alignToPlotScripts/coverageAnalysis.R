ad0f8497e5b5e789ff3a6eac434882f7024eedcd
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-06-16 12:16:03 +0930

    Initial add Script takes two arguments, one is the filename for a bedfile it will read and plot.

---
+++ alignToPlotScripts/coverageAnalysis.R
@@ -0,0 +1,45 @@
+
+setwd("~/Documents/University/Honours_2016/Project/ReadCoverage/Mut-F2-Rep1-Plots/")
+
+args = commandArgs(TRUE)
+
+# test if there is at least one argument: if not, return an error
+if (length(args)==0) {
+  stop("At least one argument must be supplied (input file).n", call.=FALSE)
+} else if (length(args)==1) {
+  # default output file
+  args[2] = "out.txt"
+}
+
+library(Rsamtools)
+library(GenomicRanges)
+
+# Reading coverage table
+
+coverage <- read.table(file = args[1])
+colnames(coverage) <- c("chromosome", "start", "stop", "read", "Unique", "Strand", "number overlapping features", "Number bases at depth", "length of read", "Fraction of read at depth")
+
+#Average depth of covered bases
+
+depthAtCovered <- (coverage$`number overlapping features`/coverage$`Number bases at depth`)*101
+
+pdf(args[2])
+plot(coverage$`Fraction of read at depth`, depthAtCovered, xlim = c(0, 1), ylim = c(0,10), 
+     main = 'Average depth of covered L1 bases', 
+     ylab = 'Depth of covered bases in L1 elements',
+     xlab = 'Proportion of L1 bases covered by at least one read')
+dev.off
+
+#file.rename( Mut-F2-Rep1_CGTACG_L007.STAR.2.20.bothorf.coverage.bed.pdf, ./ReadCoverage/Mut-F2-Rep1-Plots/Mut-F2-Rep1_CGTACG_L007.STAR.2.20.bothorf.coverage.bed.pdf)
+
+plot(coverage$`Fraction of read at depth`, depthAtCovered, xlim = c(0, .2), ylim = c(1,2.5), 
+     main = 'Average depth of covered L1 bases', 
+     ylab = 'Depth of covered bases in L1 elements',
+     xlab = 'Proportion of L1 bases covered by at least one read')
+
+
+
+# median(coverage$`Fraction of read at depth`)
+# median(coverage$`number overlapping features`)
+# sortDepthAtCovered <- sort(depthAtCovered, decreasing = FALSE)
+# median(sortDepthAtCovered)

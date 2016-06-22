9c06238033d16c4cb2dc35d3331f53a82a0e7568
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-06-22 09:43:51 +0930

    Added 4 summary plots Makes 4 plots which show the change in coverage for different multimapping and mismatching values

--- alignToPlotScripts/makeCoverageSummary.R
+++ alignToPlotScripts/makeCoverageSummary.R
@@ -19,46 +19,48 @@ plot(meansDF$fractionCovered, meansDF$depthAtCovered,
      main = 'Average depth of covered L1 bases', 
      ylab = 'Mean depth of covered bases in L1 elements',
      xlab = 'Mean proportion of L1 bases covered by at least one read' )
-dev.off
+dev.off()
 
-head(meansDF)
 
 # Playing with plots
-pdf()
+pdf("~/Documents/University/Honours_2016/Project/ReadCoverage/Mut-F2-Rep1-Plots/Summary-MismatchFraction.pdf", width=12, height=8)
 plot(meansDF$Mismatches, meansDF$fractionCovered, 
-     main = 'Average depth of covered L1 bases', 
      ylab = 'Mean depth of covered bases in L1 elements',
      xlab = 'Number of allowed mismatches', type = 'n')
 text(meansDF$Mismatches, meansDF$fractionCovered, paste(meansDF$Mismatches, meansDF$MultiMaps, sep=", "), cex=0.8)
+dev.off()
 
+pdf("~/Documents/University/Honours_2016/Project/ReadCoverage/Mut-F2-Rep1-Plots/Summary-MultimapFraction.pdf", width=12, height=8)
 plot(meansDF$MultiMaps, meansDF$fractionCovered, 
-     main = 'Average depth of covered L1 bases', 
      ylab = 'Mean depth of covered bases in L1 elements',
      xlab = 'Number of reads allowed to map to the same coordinate', type = 'n')
 text(meansDF$MultiMaps, meansDF$fractionCovered, paste(meansDF$Mismatches, meansDF$MultiMaps, sep=", "), cex=0.8)
+dev.off()
 
+pdf("~/Documents/University/Honours_2016/Project/ReadCoverage/Mut-F2-Rep1-Plots/Summary-MismatchDepth.pdf", width=12, height=8)
 plot(meansDF$Mismatches, meansDF$depthAtCovered, 
-     main = 'Average depth of covered L1 bases', 
      ylab = 'Mean depth of covered bases in L1 elements',
      xlab = 'Number of allowed mismatches', type = 'n')
 text(meansDF$Mismatches, meansDF$depthAtCovered, paste(meansDF$Mismatches, meansDF$MultiMaps, sep=", "), cex=0.8)
+dev.off()
 
+pdf("~/Documents/University/Honours_2016/Project/ReadCoverage/Mut-F2-Rep1-Plots/Summary-MultiFraction.pdf", width=12, height=8)
 plot(meansDF$MultiMaps, meansDF$depthAtCovered, 
-     main = 'Average depth of covered L1 bases', 
      ylab = 'Mean depth of covered bases in L1 elements',
      xlab = 'Number of reads allowed to map to the same coordinate', type = 'n')
 text(meansDF$MultiMaps, meansDF$depthAtCovered, paste(meansDF$Mismatches, meansDF$MultiMaps, sep=", "), cex=0.8)
+dev.off()
 
-BestWorst <- read.table("./ReadCoverage/Mut-F2-Rep-1NotPlots/SummaryBestTable.txt")
-colnames(BestWorst) <- c("Mismatch", "Multimap", "Proportion at depth", "Mean depth of covered bases" )
+#BestWorst <- read.table("./ReadCoverage/Mut-F2-Rep-1NotPlots/SummaryBestTable.txt")
+#colnames(BestWorst) <- c("Mismatch", "Multimap", "Proportion at depth", "Mean depth of covered bases" )
 
-dBestWorst <- density(BestWorst$Multimap)
-plot(dBestWorst)
+#dBestWorst <- density(BestWorst$Multimap)
+#plot(dBestWorst)
 
-plot(BestWorst$`Proportion at depth`, BestWorst$`Mean depth of covered bases`, type = 'n',
-     main = 'Mismatch and multimapping parameters for the highest and lowest values', 
-     ylab = 'Mean depth of covered bases in L1 elements',
-     xlab = 'Mean proportion of L1 bases covered by at least one read' )
-text(BestWorst$`Proportion at depth`, BestWorst$`Mean depth of covered bases`, paste(round(BestWorst$Mismatch, 2), round(BestWorst$Multimap, 2), sep=", "), cex=0.8)
+#plot(BestWorst$`Proportion at depth`, BestWorst$`Mean depth of covered bases`, type = 'n',
+ #    main = 'Mismatch and multimapping parameters for the highest and lowest values', 
+ #    ylab = 'Mean depth of covered bases in L1 elements',
+#     xlab = 'Mean proportion of L1 bases covered by at least one read' )
+#text(BestWorst$`Proportion at depth`, BestWorst$`Mean depth of covered bases`, paste(round(BestWorst$Mismatch, 2), round(BestWorst$Multimap, 2), sep=", "), cex=0.8)
 
 

30e2a80892ea8915323d8f7dbfe3c3dce84c69b9
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-06-23 17:02:13 +0930

    Changed for ORF2 only files Changed ADEDATE as it prints the same date as was set when the variable was. Changed the error tracking to be the most informative possible. Changed location file and name of output files, obviously.

--- alignToPlotScripts/Intersect.sh
+++ alignToPlotScripts/Intersect.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-## Script converts bam files to BEDs and then intersects them with L1 locations
+## Script intersects them with L1 locations
 ## Date: 10-6-2016
 
 # Working folders
 BEDDir=/mnt/project/Data/Mouse/BED/Mut-F2-Rep1_CGTACG_L007
-record=/mnt/project/Data/Mouse/Scripts/record/recordIntersectRep1.txt 
 COORDDIR=/mnt/project/Data/Mouse/locationMouse
+COORD=L1_Mouse_orf2only.bed
 OUTDIR=/mnt/project/Data/Mouse/intersect/Mut-F2-Rep1_CGTACG_L007
-error=/mnt/project/Data/Mouse/Scripts/record/recordBAMtoBEDRep1.err.log 
-ADEDATE=$(TZ="Australia/Adelaide" date)
+recordDIR=/mnt/project/Coverage/Scripts/record/
+record=/mnt/project/Data/Mouse/Scripts/record/recordIntersectRep1-ORF2.txt 
+error=/mnt/project/Data/Mouse/Scripts/record/recordIntersectRep1-ORF2.err
+RecordError=/mnt/project/Data/Mouse/Scripts/record/recordIntersectRep1-ORF2.err.log
 
 # Check and make record
 if [ -f ${record} ]; then
@@ -22,7 +24,7 @@ fi
 
 # Check and make error.log
 echo "Commencing program">> ${record} 2>&1
-echo $ADEDATE  >> ${record} 2>&1
+TZ=Australia/Adelaide date  >> ${record} 2>&1
 
 if [ -f ${error} ]; then
 	rm ${error}
@@ -31,6 +33,16 @@ if [ -f ${error} ]; then
 else
     touch ${error}
     echo "${error} did not exist, now does" >> ${record} 2>&1
+fi
+
+# Check and make RecordError
+if [ -f ${RecordError} ]; then
+	rm ${RecordError}
+	touch ${RecordError}
+    echo "${RecordError} exists, replacing" >> ${record} 2>&1
+else
+    touch ${RecordError}
+    echo "${RecordError} did not exist, now does" >> ${record} 2>&1
 fi
 
 # Check OUTDIR exists 
@@ -50,12 +62,24 @@ for file in *.bed ; do
 filename=${file%.bed}
 	
 	echo "Commencing intersect for ${filename}" >> ${record} 2>&1
-	bedtools intersect -a  ${BEDDir}/${file} -b  ${COORDDIR}/L1_Mouse_bothorf.bed > ${OUTDIR}/${filename}-bothorf.bed 2> ${record}
+	bedtools intersect -a  ${BEDDir}/${file} -b  ${COORDDIR}/${COORD} > ${OUTDIR}/${filename}-ORF2only.bed 2> ${error}
 
 done
 
+# Make combined record file
+cd ${recordDIR}
+echo "Record file:" >> ${RecordError}
+cat recordIntersectRep1-ORF2.txt >> ${RecordError}
+echo "" >> ${RecordError}
+echo "Error file:" >> ${RecordError}
+cat recordIntersectRep1-ORF2.err>> ${RecordError}
+
+
+
 echo "Completed program" >> ${record} 2>&1
-echo $ADEDATE  >> ${record} 2>&1
+TZ=Australia/Adelaide date  >> ${record} 2>&1
 
 cat ${record} | mail -s "Finished Intersect" brittany.howell1@gmail.com 
 	echo "Email sent"  >> ${record} 2>&1
+
+

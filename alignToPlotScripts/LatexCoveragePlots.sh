926d3519b114d6e6afce4b7f54eb4c8fe78e4359
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-06-24 14:18:36 +0930

    Changed for merge data

--- alignToPlotScripts/LatexCoveragePlots.sh
+++ alignToPlotScripts/LatexCoveragePlots.sh
@@ -1,17 +1,17 @@
 #/bin/bash
-PlotsDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/ReadCoverage/Mut-F2-Rep1-Plots
+PlotsDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/ReadCoverage/Mut-F2-Rep1-merge-Plots
 
 
 cd ${PlotsDIR}
 
 # Check if tablefile exists
-if [ -f coveragePlots.tex ]; then
-	rm coveragePlots.tex
-	touch coveragePlots.tex
-    echo "coveragePlots.tex exists....replacing" 
+if [ -f coveragePlotsMerge.tex ]; then
+	rm coveragePlotsMerge.tex
+	touch coveragePlotsMerge.tex
+    echo "coveragePlotsMerge.tex exists....replacing" 
 else
-    touch coveragePlots.tex
-    echo "coveragePlots.tex did not exist....creating" 
+    touch coveragePlotsMerge.tex
+    echo "coveragePlotsMerge.tex did not exist....creating" 
 fi
 
 # Make tablefile
@@ -20,15 +20,23 @@ for iPlot in *zoom.pdf ; do
 filename=${iPlot%-zoom.pdf}
 captionName=$(echo $filename | sed 's/_/-/g')
 
-	echo "\begin{figure}[tbp]" >> coveragePlots.tex
-	echo "\centering"  >> coveragePlots.tex
-	echo "\makebox[\textwidth][c]{" >> coveragePlots.tex
-	echo "\includegraphics[width=0.6\linewidth]{/Users/brittanyhowell/Documents/University/Honours_2016/Project/ReadCoverage/Mut-F2-Rep1-Plots/Mut-F2-Rep1_CGTACG_L007-STAR-10-20-bothorf.pdf}" >> coveragePlots.tex
-	echo "\includegraphics[width=0.6\linewidth]{/Users/brittanyhowell/Documents/University/Honours_2016/Project/ReadCoverage/Mut-F2-Rep1-Plots/Mut-F2-Rep1_CGTACG_L007-STAR-10-20-bothorf-zoom.pdf}}" >> coveragePlots.tex
-	echo "\caption{${captionName}}" >> coveragePlots.tex
-	echo "\label{fig:coverage-${captionName}}" >> coveragePlots.tex
-	echo "\end{figure}" >> coveragePlots.tex
-	echo ""
+	echo "\begin{figure}[tbp]" >> coveragePlotsMerge.tex
+	echo "\centering"  >> coveragePlotsMerge.tex
+	echo "\makebox[\textwidth][c]{" >> coveragePlotsMerge.tex
+	echo "\begin{subfigure}[b]{.59\textwidth}" >> coveragePlotsMerge.tex
+	echo "\includegraphics[width=\linewidth]{/Users/brittanyhowell/Documents/University/Honours_2016/Project/ReadCoverage/Mut-F2-Rep1-Plots/Mut-F2-Rep1_CGTACG_L007-STAR-10-20-bothorf.pdf}" >> coveragePlotsMerge.tex
+	echo "\vspace{-1cm}" >> coveragePlotsMerge.tex
+	echo "\caption{Average depth and proportion of L1 covered}" >> coveragePlotsMerge.tex
+	echo "\end{subfigure}" >> coveragePlotsMerge.tex
+	echo "\begin{subfigure}[b]{.59\textwidth}" >> coveragePlotsMerge.tex
+	echo "\includegraphics[width=\linewidth]{/Users/brittanyhowell/Documents/University/Honours_2016/Project/ReadCoverage/Mut-F2-Rep1-Plots/Mut-F2-Rep1_CGTACG_L007-STAR-10-20-bothorf-zoom.pdf}" >> coveragePlotsMerge.tex
+	echo "\vspace{-1cm}" >> coveragePlotsMerge.tex
+	echo "\caption{Enlarged lower left quartile of (a)}" >> coveragePlotsMerge.tex
+	echo "\end{subfigure}}" >> coveragePlotsMerge.tex
+	echo "\caption{${captionName}}" >> coveragePlotsMerge.tex
+	echo "\label{fig:coverage-${captionName}}" >> coveragePlotsMerge.tex
+	echo "\end{figure}" >> coveragePlotsMerge.tex                                                                                                                                                                                                                                                                                                                                                                            
+	echo "" >> coveragePlotsMerge.tex
 
 done
 
--- alignToPlotScripts/LatexCoverageTable.sh
+++ alignToPlotScripts/LatexCoverageTable.sh
@@ -1,35 +1,35 @@
 #/bin/bash
-PlotsDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/ReadCoverage/Mut-F2-Rep1-Plots
+PlotsDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/ReadCoverage/Mut-F2-Rep1-merge-Plots
 
 
 cd ${PlotsDIR}
 
 # Check if tablefile exists
-if [ -f SummaryTableLatex.tex ]; then
-	rm SummaryTableLatex.tex
-	touch SummaryTableLatex.tex
-    echo "SummaryTableLatex.tex exists....replacing" 
+if [ -f SummaryTableMergeLatex.tex ]; then
+	rm SummaryTableMergeLatex.tex
+	touch SummaryTableMergeLatex.tex
+    echo "SummaryTableMergeLatex.tex exists....replacing" 
 else
-    touch SummaryTableLatex.tex
-    echo "SummaryTableLatex.tex did not exist....creating" 
+    touch SummaryTableMergeLatex.tex
+    echo "SummaryTableMergeLatex.tex did not exist....creating" 
 fi
 
 # Make Full table
-echo "\begin{longtable}[c]{|l|l|p{5.5cm}|p{5.5cm}|}" >> SummaryTableLatex.tex
-echo "\caption{Summary coverage values for alignments. Mean fraction of read at non-zero depth represents the average proportion of covered L1s. Mean depth at covered bases refers to the average depth of the L1 bases which are covered by a read.} \\\\" >> SummaryTableLatex.tex
-echo "\hline" >> SummaryTableLatex.tex
-echo "Mismatches & Multi-mapping & Mean fraction of read at non-zero depth & Mean depth of covered bases \\\\" >> SummaryTableLatex.tex
-echo "\hline\hline \endfirsthead" >>SummaryTableLatex.tex
-echo "\hline" >> SummaryTableLatex.tex
-echo "Mismatches & Multi-mapping & Mean fraction of read at non-zero depth & Mean depth of covered bases \\\\" >> SummaryTableLatex.tex
-echo "\hline\hline \endhead" >>SummaryTableLatex.tex
-echo "\hline \endfoot" >> SummaryTableLatex.tex
-sed -n 2,31p SummaryTable.txt | awk '{print $2 "\t" "&" "\t" $3 "\t" "&" "\t" $4 "\t" "&" "\t" $5 "\t" "\\\\"}' >> SummaryTableLatex.tex
-echo "\end{longtable}" >> SummaryTableLatex.tex
+echo "\begin{longtable}[c]{|l|l|p{5.5cm}|p{5.5cm}|}" >> SummaryTableMergeLatex.tex
+echo "\caption{Summary coverage values for alignments. Mean fraction of read at non-zero depth represents the average proportion of covered L1s. Mean depth at covered bases refers to the average depth of the L1 bases which are covered by a read.} \\\\" >> SummaryTableMergeLatex.tex
+echo "\hline" >> SummaryTableMergeLatex.tex
+echo "Mismatches & Multi-mapping & Mean fraction of read at non-zero depth & Mean depth of covered bases \\\\" >> SummaryTableMergeLatex.tex
+echo "\hline\hline \endfirsthead" >>SummaryTableMergeLatex.tex
+echo "\hline" >> SummaryTableMergeLatex.tex
+echo "Mismatches & Multi-mapping & Mean fraction of read at non-zero depth & Mean depth of covered bases \\\\" >> SummaryTableMergeLatex.tex
+echo "\hline\hline \endhead" >>SummaryTableMergeLatex.tex
+echo "\hline \endfoot" >> SummaryTableMergeLatex.tex
+sed -n 2,31p SummaryTable.txt | awk '{print $2 "\t" "&" "\t" $3 "\t" "&" "\t" $4 "\t" "&" "\t" $5 "\t" "\\\\"}' >> SummaryTableMergeLatex.tex
+echo "\end{longtable}" >> SummaryTableMergeLatex.tex
 
 
 # Make summary table
-#sed -n 10,39p SummaryTableLatex.tex | awk '{print $1 "\t" $3 "\t"  $5 "\t" $7}' > basicTEXSummTable.txt
+#sed -n 10,39p SummaryTableMergeLatex.tex | awk '{print $1 "\t" $3 "\t"  $5 "\t" $7}' > basicTEXSummTable.txt
 
 
 

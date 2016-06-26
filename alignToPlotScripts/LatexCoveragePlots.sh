6f725bcd5e1ac174a9c4936a3eba2c5476c0dc98
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-06-26 17:04:14 +0930

    Update for ext coverage changed includegraphics filenames and added arguments

--- alignToPlotScripts/LatexCoveragePlots.sh
+++ alignToPlotScripts/LatexCoveragePlots.sh
@@ -1,17 +1,19 @@
 #/bin/bash
-PlotsDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/ReadCoverage/Mut-F2-Rep1-merge-Plots
+PlotsDIR=$1
+
+
 
 
 cd ${PlotsDIR}
 
 # Check if tablefile exists
-if [ -f coveragePlotsMerge.tex ]; then
-	rm coveragePlotsMerge.tex
-	touch coveragePlotsMerge.tex
-    echo "coveragePlotsMerge.tex exists....replacing" 
+if [ -f coveragePlots.tex ]; then
+	rm coveragePlots.tex
+	touch coveragePlots.tex
+    echo "coveragePlots.tex exists....replacing" 
 else
-    touch coveragePlotsMerge.tex
-    echo "coveragePlotsMerge.tex did not exist....creating" 
+    touch coveragePlots.tex
+    echo "coveragePlots.tex did not exist....creating" 
 fi
 
 # Make tablefile
@@ -20,63 +22,22 @@ for iPlot in *zoom.pdf ; do
 filename=${iPlot%-zoom.pdf}
 captionName=$(echo $filename | sed 's/_/-/g')
 
-	echo "\begin{figure}[tbp]" >> coveragePlotsMerge.tex
-	echo "\centering"  >> coveragePlotsMerge.tex
-	echo "\makebox[\textwidth][c]{" >> coveragePlotsMerge.tex
-	echo "\begin{subfigure}[b]{.59\textwidth}" >> coveragePlotsMerge.tex
-	echo "\includegraphics[width=\linewidth]{/Users/brittanyhowell/Documents/University/Honours_2016/Project/ReadCoverage/Mut-F2-Rep1-Plots/Mut-F2-Rep1_CGTACG_L007-STAR-10-20-bothorf.pdf}" >> coveragePlotsMerge.tex
-	echo "\vspace{-1cm}" >> coveragePlotsMerge.tex
-	echo "\caption{Average depth and proportion of L1 covered}" >> coveragePlotsMerge.tex
-	echo "\end{subfigure}" >> coveragePlotsMerge.tex
-	echo "\begin{subfigure}[b]{.59\textwidth}" >> coveragePlotsMerge.tex
-	echo "\includegraphics[width=\linewidth]{/Users/brittanyhowell/Documents/University/Honours_2016/Project/ReadCoverage/Mut-F2-Rep1-Plots/Mut-F2-Rep1_CGTACG_L007-STAR-10-20-bothorf-zoom.pdf}" >> coveragePlotsMerge.tex
-	echo "\vspace{-1cm}" >> coveragePlotsMerge.tex
-	echo "\caption{Enlarged lower left quartile of (a)}" >> coveragePlotsMerge.tex
-	echo "\end{subfigure}}" >> coveragePlotsMerge.tex
-	echo "\caption{${captionName}}" >> coveragePlotsMerge.tex
-	echo "\label{fig:coverage-${captionName}}" >> coveragePlotsMerge.tex
-	echo "\end{figure}" >> coveragePlotsMerge.tex                                                                                                                                                                                                                                                                                                                                                                            
-	echo "" >> coveragePlotsMerge.tex
+	echo "\begin{figure}[tbp]" >> coveragePlots.tex
+	echo "\centering"  >> coveragePlots.tex
+	echo "\makebox[\textwidth][c]{" >> coveragePlots.tex
+	echo "\begin{subfigure}[b]{.59\textwidth}" >> coveragePlots.tex
+	echo "\includegraphics[width=\linewidth]{/Users/brittanyhowell/Documents/University/Honours_2016/Project/ReadCoverage/Mut-F2-Rep1-Plots/${filename}.pdf}" >> coveragePlots.tex
+	echo "\vspace{-1cm}" >> coveragePlots.tex
+	echo "\caption{Average depth and proportion of L1 covered}" >> coveragePlots.tex
+	echo "\end{subfigure}" >> coveragePlots.tex
+	echo "\begin{subfigure}[b]{.59\textwidth}" >> coveragePlots.tex
+	echo "\includegraphics[width=\linewidth]{/Users/brittanyhowell/Documents/University/Honours_2016/Project/ReadCoverage/Mut-F2-Rep1-Plots/${filename}-zoom.pdf}" >> coveragePlots.tex
+	echo "\vspace{-1cm}" >> coveragePlots.tex
+	echo "\caption{Enlarged lower left quartile of (a)}" >> coveragePlots.tex
+	echo "\end{subfigure}}" >> coveragePlots.tex
+	echo "\caption{${captionName}}" >> coveragePlots.tex
+	echo "\label{fig:coverage-${captionName}}" >> coveragePlots.tex
+	echo "\end{figure}" >> coveragePlots.tex                                             
+	echo "" >> coveragePlots.tex
 
 done
-
-# PlotsDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/ReadCoverage/Mut-F2-Rep1-Plots
-
-
-# cd ${PlotsDIR}
-
-# # Check if tablefile exists
-# if [ -f coveragePlots.tex ]; then
-# 	rm coveragePlots.tex
-# 	touch coveragePlots.tex
-#     echo "coveragePlots.tex exists....replacing" 
-# else
-#     touch coveragePlots.tex
-#     echo "coveragePlots.tex did not exist....creating" 
-# fi
-
-# # Make tablefile
-# for iPlot in *zoom.pdf ; do 
-
-# filename=${iPlot%-zoom.pdf}
-# captionName=$(echo $filename | sed 's/_/-/g')
-
-# 	echo "\begin{figure}[tbp]" >> coveragePlots.tex
-# 	echo "\centering"  >> coveragePlots.tex
-# 	echo "\makebox[\textwidth][c]{" >> coveragePlots.tex
-# 	echo "\begin{subfigure}[b]{.59\textwidth}" >> coveragePlots.tex
-# 	echo "\includegraphics[width=\linewidth]{/Users/brittanyhowell/Documents/University/Honours_2016/Project/ReadCoverage/Mut-F2-Rep1-Plots/Mut-F2-Rep1_CGTACG_L007-STAR-10-20-bothorf.pdf}" >> coveragePlots.tex
-# 	echo "\vspace{-1cm}" >> coveragePlots.tex
-# 	echo "\caption{Average depth and proportion of L1 covered}" >> coveragePlots.tex
-# 	echo "\end{subfigure}" >> coveragePlots.tex
-# 	echo "\begin{subfigure}[b]{.59\textwidth}" >> coveragePlots.tex
-# 	echo "\includegraphics[width=\linewidth]{/Users/brittanyhowell/Documents/University/Honours_2016/Project/ReadCoverage/Mut-F2-Rep1-Plots/Mut-F2-Rep1_CGTACG_L007-STAR-10-20-bothorf-zoom.pdf}" >> coveragePlots.tex
-# 	echo "\vspace{-1cm}" >> coveragePlots.tex
-# 	echo "\caption{Enlarged lower left quartile of (a)}" >> coveragePlots.tex
-# 	echo "\end{subfigure}}" >> coveragePlots.tex
-# 	echo "\caption{${captionName}}" >> coveragePlots.tex
-# 	echo "\label{fig:coverage-${captionName}}" >> coveragePlots.tex
-# 	echo "\end{figure}" >> coveragePlots.tex                                                                                                                                                                                                                                                                                                                                                                            
-# 	echo "" >> coveragePlots.tex
-
-# done
--- alignToPlotScripts/LatexCoverageSummaryPlot.sh
+++ alignToPlotScripts/LatexCoverageSummaryPlot.sh
@@ -1,44 +1,21 @@
 #/bin/bash
-PlotsDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/ReadCoverage/Mut-F2-Rep1-merge-Plots
+PlotsDIR=$1
 
 cd ${PlotsDIR}
 
 # Check if tablefile exists
-if [ -f SummaryPlotMerge.tex ]; then
-	rm SummaryPlotMerge.tex
-	touch SummaryPlotMerge.tex
-    echo "SummaryPlotMerge.tex exists....replacing" 
+if [ -f SummaryPlot.tex ]; then
+	rm SummaryPlot.tex
+	touch SummaryPlot.tex
+    echo "SummaryPlot.tex exists....replacing" 
 else
-    touch SummaryPlotMerge.tex
-    echo "SummaryPlotMerge.tex did not exist....creating" 
+    touch SummaryPlot.tex
+    echo "SummaryPlot.tex did not exist....creating" 
 fi
 
-	echo "\begin{figure}[tbp]" >> SummaryPlotMerge.tex
-	echo "\centering"  >> SummaryPlotMerge.tex
-	echo "\includegraphics[width=0.6\linewidth]{/Users/brittanyhowell/Documents/University/Honours_2016/Project/ReadCoverage/Mut-F2-Rep1-Plots/SummaryPlot.pdf}" >> SummaryPlotMerge.tex
-	echo "\caption{Summary of coverage}" >> SummaryPlotMerge.tex
-	echo "\label{fig:SummCoverage}" >> SummaryPlotMerge.tex
-	echo "\end{figure}" >> SummaryPlotMerge.tex
-
-
-# PlotsDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/ReadCoverage/Mut-F2-Rep1-Plots
-
-
-# cd ${PlotsDIR}
-
-# # Check if tablefile exists
-# if [ -f SummaryPlot.tex ]; then
-# 	rm SummaryPlot.tex
-# 	touch SummaryPlot.tex
-#     echo "SummaryPlot.tex exists....replacing" 
-# else
-#     touch SummaryPlot.tex
-#     echo "SummaryPlot.tex did not exist....creating" 
-# fi
-
-# 	echo "\begin{figure}[tbp]" >> SummaryPlot.tex
-# 	echo "\centering"  >> SummaryPlot.tex
-# 	echo "\includegraphics[width=0.6\linewidth]{/Users/brittanyhowell/Documents/University/Honours_2016/Project/ReadCoverage/Mut-F2-Rep1-Plots/SummaryPlot.pdf}" >> SummaryPlot.tex
-# 	echo "\caption{Summary of coverage}" >> SummaryPlot.tex
-# 	echo "\label{fig:SummCoverage}" >> SummaryPlot.tex
-# 	echo "\end{figure}" >> SummaryPlot.tex
+	echo "\begin{figure}[tbp]" >> SummaryPlot.tex
+	echo "\centering"  >> SummaryPlot.tex
+	echo "\includegraphics[width=\linewidth]{/Users/brittanyhowell/Documents/University/Honours_2016/Project/ReadCoverage/Mut-F2-Rep1-Plots/SummaryPlot.pdf}" >> SummaryPlot.tex
+	echo "\caption{Summary of coverage}" >> SummaryPlot.tex
+	echo "\label{fig:SummCoverage}" >> SummaryPlot.tex
+	echo "\end{figure}" >> SummaryPlot.tex

61d38f51da221d1ad59909edbf799b43f1499c71
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-06-26 17:54:48 +0930

    Moves more Now moves parameter tables from VM as well as coverage data

--- alignToPlotScripts/moveCoverageBED.sh
+++ alignToPlotScripts/moveCoverageBED.sh
@@ -3,8 +3,13 @@
 
 # Data folders
 BED=Mut-F2-Rep1_CGTACG_L007-ext/*.bed
-OUTDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/ReadCoverage/Mut-F2-Rep1-ext/*
+COVDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/ReadCoverage/Mut-F2-Rep1-ext/*
+ParamDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/Genomes/alignToPlotScripts/ParameterTables
 
-echo "moving coverage files"
-scp brittany\@130.220.211.67:/mnt/project/Coverage/${BED} ${OUTDIR}
+# echo "Moving coverage files"
+scp brittany\@130.220.211.67:/mnt/project/Coverage/${BED} ${COVDIR}
+
+echo "Moving parameter files"
+scp brittany\@130.220.211.67:/mnt/project/Data/Mouse/extendedAlignmentOutputData/Parameters/* ${ParamDIR}
+
 echo "Completed moving files"

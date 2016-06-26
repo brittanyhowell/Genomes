7d50025b274be4b1408293cd10e40fa41dad13ab
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-06-26 18:07:32 +0930

    Removal of files

--- runSTAR.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-STAR --genomeDir ../mouse/indexed/ --readFilesIn Mut-F2-Rep1_CGTACG_L007_R1.fastq Mut-F2-Rep1_CGTACG_L007_R2.fastq --outFilterMismatchNmax 2 --outFilterMultimapNmax 20 --outFileNamePrefix ../mouse/output/mm10 --outSAMstrandField intronMotif --outSAMtype BAM SortedByCoordinate --alignSoftClipAtReferenceEnds No 
-
-
-
-
-# Removed outSAMattributes All - because I'm not sure it "All" is correct

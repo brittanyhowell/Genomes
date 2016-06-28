413edc72f74fa94e894e34ad64432fbad802aaab
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-06-28 16:58:36 +0930

    Added variables

--- alignToPlotScripts/moveCoverageBED.sh
+++ alignToPlotScripts/moveCoverageBED.sh
@@ -2,14 +2,15 @@
 ## moveCoverageBED.sh moves all BEDs from a folder into the necessary place
 
 # Data folders
-BED=Mut-F2-Rep1_CGTACG_L007-ext/*.bed
-COVDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/ReadCoverage/Mut-F2-Rep1-ext/*
+BEDDIR=/mnt/project/Coverage/Mut-F2-Rep1_CGTACG_L007-TEMP/*
+COVDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/ReadCoverage/Mut-F2-Rep1-ext
+Param=/mnt/project/Data/Mouse/tempBAMS/someextendedAlignmentOutputData/Parameters/*
 ParamDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/Genomes/alignToPlotScripts/ParameterTables
 
-# echo "Moving coverage files"
-scp brittany\@130.220.211.67:/mnt/project/Coverage/${BED} ${COVDIR}
+ echo "Moving coverage files"
+ scp brittany\@130.220.211.67:${BEDDIR} ${COVDIR}
 
 echo "Moving parameter files"
-scp brittany\@130.220.211.67:/mnt/project/Data/Mouse/extendedAlignmentOutputData/Parameters/* ${ParamDIR}
+scp brittany\@130.220.211.67:${Param} ${ParamDIR}
 
 echo "Completed moving files"

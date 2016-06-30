abc8e2e9519c156c09c1e19cec40bdad3758a2f8
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-06-30 12:43:22 +0930

    Change numbers Change number of arguments, changed dimensions of plots.

--- alignToPlotScripts/makeCoverageSummary.R
+++ alignToPlotScripts/makeCoverageSummary.R
@@ -24,47 +24,49 @@ meansDF <- as.data.frame(means)
 
 write.table(means, file = args[3])
 
-pdf(args[4], width = 10, height = 8)
-plot(meansDF$depthAtCovered, meansDF$fractionCovered,
+pdf(args[5], width = 10, height = 6)
+plot(meansDF$depthAtCovered, meansDF$fractionCovered, # xlim = c(1.108,1.11), ylim = c(.304,.3043), 
      main = 'Average depth of covered L1 bases',
-     ylab = 'Mean depth of covered bases in L1 elements',
-     xlab = 'Mean proportion of L1 bases covered by at least one read', type = 'n')
+     ylab = 'Mean proportion of L1 bases covered by at least one read',
+     xlab = 'Mean depth of covered bases in L1 elements', type = 'n')
 text(meansDF$depthAtCovered, meansDF$fractionCovered, paste(meansDF$Mismatches, meansDF$MultiMaps, sep=", "), cex=1.1)
 graphics.off()
 
-pdf(args[5])
+pdf(args[4], width = 10, height = 5)
 plot(meansDF$fractionCovered, meansDF$depthAtCovered, 
      main = 'Average depth of covered L1 bases',
-     ylab = 'Mean depth of covered bases in L1 elements',
-     xlab = 'Mean proportion of L1 bases covered by at least one read', type = 'n')
-text(meansDF$fractionCovered, meansDF$depthAtCovered, paste(meansDF$Mismatches, meansDF$MultiMaps, sep=", "), cex=1.1)
+     xlab= 'Mean proportion of L1 bases covered by at least one read',
+     ylab = 'Mean depth of covered bases in L1 elements')
 graphics.off()
 
 # Playing with plots
-pdf(args[6], width=6, height=6)
-plot(meansDF$Mismatches, meansDF$fractionCovered, xlim = c(0,21),
+pdf(args[6], width=7, height=6)
+plot(meansDF$Mismatches, meansDF$fractionCovered, xlim = c(0,21), 
      ylab = 'Mean proportion covered',
      xlab = 'Number of allowed mismatches', type = 'n')
 text(meansDF$Mismatches, meansDF$fractionCovered, paste(meansDF$Mismatches, meansDF$MultiMaps, sep=", "), cex=1.1)
 graphics.off()
 
-pdf(args[7], width=6, height=6)
-plot(meansDF$MultiMaps, meansDF$fractionCovered, xlim = c(19,46),
+pdf(args[7], width=7, height=6)
+plot(meansDF$MultiMaps, meansDF$fractionCovered, xlim = c(19,61), 
      ylab = 'Mean proportion covered',
-     xlab = 'Number of reads allowed to map to the same coordinate', type = 'n')
+     xlab = 'Threshold number of locations to which a single read can align', type = 'n')
 text(meansDF$MultiMaps, meansDF$fractionCovered, paste(meansDF$Mismatches, meansDF$MultiMaps, sep=", "), cex=1.1)
 graphics.off()
 
-pdf(args[8], width=6, height=6)
-plot(meansDF$Mismatches, meansDF$depthAtCovered, xlim = c(0,21),
+pdf(args[8], width=7, height=6)
+plot(meansDF$Mismatches, meansDF$depthAtCovered, xlim = c(0,21), 
      ylab = 'Mean depth of covered bases in L1 elements',
      xlab = 'Number of allowed mismatches', type = 'n')
 text(meansDF$Mismatches, meansDF$depthAtCovered, paste(meansDF$Mismatches, meansDF$MultiMaps, sep=", "), cex=1.1)
 graphics.off()
 
-pdf(args[9], width=6, height=6)
-plot(meansDF$MultiMaps, meansDF$depthAtCovered, xlim = c(19,46),
+pdf(args[9], width=7, height=6)
+plot(meansDF$MultiMaps, meansDF$depthAtCovered, xlim = c(19,61), 
      ylab = 'Mean depth of covered bases in L1 elements',
-     xlab = 'Number of reads allowed to map to the same coordinate', type = 'n')
+     xlab = 'Threshold number of locations to which a single read can align', type = 'n')
 text(meansDF$MultiMaps, meansDF$depthAtCovered, paste(meansDF$Mismatches, meansDF$MultiMaps, sep=", "), cex=1.1)
 graphics.off()
+
+
+

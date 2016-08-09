30f4078f9e215916cf59520758cfc0c7df47ee07
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-08-09 14:47:06 +0930

    Changed for Phoenix Added SLURM requirements. Removed iteration over multiple mapping and mismatching values. Picked 50 and 7. Tried --genomeLoad  LoadAndKeep which should keep the genome loaded in between runs, but we will see. Added 8 threads, which should be manageable on Phoenix.

--- alignToPlotScripts/alignIndexSTAR.sh
+++ alignToPlotScripts/alignIndexSTAR.sh
@@ -1,153 +1,104 @@
 #!/bin/bash
 ## alignIndexSTAR.sh aligns mouse HENMT2 reads to mm10 using STAR,
 ## Date: 31-5-2016
+# Invoked by:
+#
+# rawDIR=/data/rc003/Brittany/Data/TranscriptomeData/Henmt1_mRNAseq_mouse/combinedRaw indexedDIR=/data/rc003/Brittany/genomes/indexedMouse alignDIR=/data/rc003/Brittany/Alignment/bamMouse alignOutDIR=/data/rc003/Brittany/Alignment/outputMouse sbatch STAR.sh
+#
 
-# Raw folders
-rawDIR=/mnt/project/Data/Mouse/rawReads
-indexedDIR=/mnt/project/Data/Mouse/indexedMouse
+#SBATCH -p batch
+#SBATCH -N 1 
+#SBATCH -n 8  				
+#SBATCH --time=0-09:00
+#SBATCH --mem=50GB 			
 
-#Output
-alignDIR=/mnt/project/Data/Mouse/extendedAlignedReads
-alignOutDIR=/mnt/project/Data/Mouse/extendedAlignmentOutputData
-
-#record
-record=/mnt/project/Data/Mouse/Scripts/record/recordAlignment2.txt 
-recordDIR=/mnt/project/Data/Mouse/Scripts/record
-
-#Parameter recording files
-parameterDIR=${alignOutDIR}/Parameters
-multimaps=${parameterDIR}/ParametersMaps.txt
-mismatches=${parameterDIR}/ParametersMatch.txt
-
-if [ -f ${record} ]; then
-	rm ${record}
-	touch ${record}
-    echo "${record} exists, replacing" >> ${record} 2>&1
-else
-    touch ${record}
-    echo "${record} did not exist, now does" >> ${record} 2>&1
-fi
+# Notification configuration 
+#SBATCH --mail-type=END                                         
+#SBATCH --mail-type=FAIL           
+#SBATCH --mail-user=brittany.howell@student.adelaide.edu.au      
 
 
-echo "The program starts at:" >> ${record} 2>&1
-TZ=Australia/Adelaide date >> ${record} 2>&1
+# Load the necessary modules
+module load BEDTools/2.25.0-foss-2015b
+module load STAR/2.5.1a-foss-2015b
+module load SAMtools/1.2-foss-2015b
+
+
+# # Raw folders
+# rawDIR=/data/rc003/Brittany/Data/TranscriptomeData/Henmt1_mRNAseq_mouse/combinedRaw
+# indexedDIR=/data/rc003/Brittany/genomes/indexedMouse
+
+# #Output
+# alignDIR=/data/rc003/Brittany/Alignment/bamMouse
+# alignOutDIR=/data/rc003/Brittany/Alignment/outputMouse
+
 
 # Check all folders exist
 
 if [ -d $rawDIR ]; then
-    echo "Folder $rawDIR exists ..." >> ${record} 2>&1
+    echo "Folder $rawDIR exists ..." 
 else
     mkdir $rawDIR
-    echo "Folder $rawDIR does not exist" >> ${record} 2>&1
+    echo "Folder $rawDIR does not exist"     
     exit
 fi
 
 if [ -d $indexedDIR ]; then
-    echo "Folder $indexedDIR exists ..." >> ${record} 2>&1
+    echo "Folder $indexedDIR exists ..." 
 else
     mkdir $indexedDIR
-    echo "Folder $indexedDIR does not exist" >> ${record} 2>&1
+    echo "Folder $indexedDIR does not exist"     
     exit
 fi
 
 if [ -d $alignDIR ]; then
-    echo "Folder $alignDIR exists ... replacing" >> ${record} 2>&1
+    echo "Folder $alignDIR exists ... replacing"     
     rm -r ${alignDIR}
     mkdir ${alignDIR}
 else
     mkdir $alignDIR
-    echo "Folder $alignDIR does not exist, making $alignDIR" >> ${record} 2>&1
+    echo "Folder $alignDIR does not exist, making $alignDIR" 
 fi
 
 if [ -d $alignOutDIR ]; then
-    echo "Folder $alignOutDIR exists ... replacing" >> ${record} 2>&1
+    echo "Folder $alignOutDIR exists ... replacing"     
     rm -r ${alignOutDIR}
     mkdir ${alignOutDIR}
 else
     mkdir $alignOutDIR
-    echo "Folder $alignOutDIR does not exist, making $alignOutDIR" >> ${record} 2>&1
+    echo "Folder $alignOutDIR does not exist, making $alignOutDIR" 
 fi
 
-if [ -d $parameterDIR ]; then
-    echo "Folder $parameterDIR exists ... replacing" >> ${record} 2>&1
-    rm -r ${parameterDIR}
-    mkdir ${parameterDIR}
-else
-    mkdir $parameterDIR
-    echo "Folder $parameterDIR does not exist, making $parameterDIR" >> ${record} 2>&1
-fi
-
-if [ -f ${multimaps} ]; then
-	rm ${multimaps}
-	touch ${multimaps}
-    echo "${multimaps} exists, replacing" >> ${record} 2>&1
-else
-    touch ${multimaps}
-    echo "${multimaps} did not exist, now does" >> ${record} 2>&1
-fi
-
-if [ -f ${mismatches} ]; then
-	rm ${mismatches}
-	touch ${mismatches}
-    echo "${mismatches} exists, replacing" >> ${record} 2>&1
-else
-    touch ${mismatches}
-    echo "${mismatches} did not exist, now does" >> ${record} 2>&1
-fi
-
-#Alignment parameters 
-MULTIMAP=( "25" "30" "35" "40" "45" "50" "60")
-MISMATCH=("2" "5" "7" "10" "12" "15" "20")
-
-echo "Commencing alignments" >> ${record} 2>&1
-TZ=Australia/Adelaide date >> ${record} 2>&1
 
 # Move into reads folder
 cd ${rawDIR}
-READ=$(ls *_R1.fastq)
+READ=$(ls *_R1*)
 
 for file in ${READ}; do
 
-	for iMap in "${MULTIMAP[@]}"; do
+	gzip -d ${file}.fastq.gz 
 
-		for iMatch in "${MISMATCH[@]}"; do
+	readname=${file%_R1.fastq}
 
-			readname=${file%_R1.fastq}
+	echo "Running STAR for ${readname}" 		
 
-			echo "Running STAR for ${readname} with ${iMap} multimapping and ${iMatch} mismatches" >> ${record} 2>&1
-			
-			echo "${iMap}" >> ${multimaps}
-			echo "${iMatch}" >> ${mismatches}
+	#Run STAR
+ STAR --runThreadN 8 --genomeDir ${indexedDIR} --genomeLoad LoadAndKeep --readFilesIn ${rawDIR}/${readname}_R1.fastq ${rawDIR}/${readname}_R2.fastq --outFilterMismatchNmax 7 --outFilterMultimapNmax 50 --outFileNamePrefix ${alignDIR}/${readname}.STAR. --outSAMstrandField intronMotif --outSAMattributes All --outSAMtype BAM SortedByCoordinate --alignSoftClipAtReferenceEnds No 
 
-			#Run STAR
-			STAR --genomeDir ${indexedDIR} --readFilesIn ${rawDIR}/${readname}_R1.fastq ${rawDIR}/${readname}_R2.fastq --outFilterMismatchNmax ${iMatch} --outFilterMultimapNmax ${iMap} --outFileNamePrefix ${alignDIR}/${readname}.STAR.${iMatch}.${iMap}. --outSAMstrandField intronMotif --outSAMattributes All --outSAMtype BAM SortedByCoordinate --alignSoftClipAtReferenceEnds No  2> align.err
+		cd ${alignDIR}
 
-	 		cd ${alignDIR}
-			# Make bam index
-			echo "Making bam index" >> ${record} 2>&1
-			samtools index ${readname}.STAR.${iMatch}.${iMap}.Aligned.sortedByCoord.out.bam 
+	# Make bam index
+	echo "Making bam index" 
+    samtools index ${readname}.STAR.Aligned.sortedByCoord.out.bam 
 
-			# Move alignment information files into separate folder
-			mv ${readname}.STAR.${iMatch}.${iMap}.Log.out ${alignOutDIR} 
-			mv ${readname}.STAR.${iMatch}.${iMap}.Log.final.out ${alignOutDIR} 
-			mv ${readname}.STAR.${iMatch}.${iMap}.Log.progress.out ${alignOutDIR} 
+	# Move alignment information files into separate folder
+	mv ${readname}.STAR.Log.out ${alignOutDIR} 
+	mv ${readname}.STAR.Log.final.out ${alignOutDIR} 
+	mv ${readname}.STAR.Log.progress.out ${alignOutDIR} 
 
-			echo "Completed alignment for ${readname} with ${iMap} multimapping and ${iMatch} mismatches" >> ${record} 2>&1
-			TZ=Australia/Adelaide date >> ${record} 2>&1
-
-			cd ${rawDIR}
-
-			cat ${record} | mail -s "finished Alignment for map: ${iMap} and mismatch: ${iMatch}" brittany.howell1@gmail.com
-		done
-
-	done
-
-	cd ${recordDIR}
-	
-	cat ${record} | mail -s "finished all Alignments" brittany.howell1@gmail.com 
-	echo "Email sent"  >> ${record} 2>&1
+	# Zip fasta file
+	echo "Zipping read file"
+	gzip ${file}
 
 done
 
-echo "Exiting program" >> ${record} 2>&1
-echo "Complete"

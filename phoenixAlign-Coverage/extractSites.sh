ecbdf6532b55d2c25ae3cf2c19a6cc4db9f90c70
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-10-08 15:49:42 +1030

    New add

---
+++ phoenixAlign-Coverage/extractSites.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+## Script extracts column containing splice acceptor sites from bamReader.go output
+
+wkDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/bamReading/Split/limitedGap
+outDIR=${wkDIR}/SpliceSites
